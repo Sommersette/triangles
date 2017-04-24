c32858b7fe3e530e4b4e1e17f8c13ac3fe952015
Kai Leahy and Trevor Fast <trevor@>
2017-04-24 10:38:41 -0700

    add  code to compare triangles: equilateral, isoceles, and scalene triangles.
    add code to determine if lengths entered actually is a triangle.

--- lib/ruby_triangles.rb
+++ lib/ruby_triangles.rb
@@ -1,5 +1,24 @@
 require "ruby_triangles/version"
+require 'pry'
 
 module RubyTriangles
+  class Triangle
+    def test(sides)
+      if is_triangle?(sides)
+        {
+          1 => 'equilateral',
+          2 => 'isoceles',
+          3 => 'scalene'
+        }.fetch(sides.uniq.length)
+      else
+        return 'Error: not a triangle'
+      end
+    end
 
+    def is_triangle?(sides)
+      sorted = sides.sort
+      max = sorted.pop
+      max <= sorted.reduce(&:+)
+    end
+  end
 end
